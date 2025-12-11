3994336d5253f984b63de32423d278924e09f9c2
Prithvi Reddy <prithvireddy0770@gmail.com>
2025-12-11 14:30:27 +0000

    fixed a bug

--- apps/web/app/room/[slug]/page.tsx
+++ apps/web/app/room/[slug]/page.tsx
@@ -4,7 +4,7 @@ import { ChatRoom } from "../../../components/ChatRoom";
 
 async function getRoomId(slug: string) {
     const response = await axios.get(`${BACKEND_URL}/room/${slug}`)
-    return response.data.roomId
+    return response.data.room.id
 }
 
 export default async function RoomPage({
